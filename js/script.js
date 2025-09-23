167cde40a33e21360a4b4c8a762b2a0230bf5a24
finvest2005 <finvest2005@yandex.ru>
2025-09-23 22:41:18 +0300

    Add hoover effect and mouse pointer on all buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container');
 const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
 container.addEventListener('click', function (e) {
   const elem = e.target;
   if (elem.className == 'btn') {
